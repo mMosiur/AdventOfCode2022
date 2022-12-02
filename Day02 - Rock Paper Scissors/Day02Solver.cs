96d1f94697769f5a4e2f825de6245092bd00e80d
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-12-02 07:22:34 +0100

    Refactor day 2

--- Day02 - Rock Paper Scissors/Day02Solver.cs
+++ Day02 - Rock Paper Scissors/Day02Solver.cs
@@ -1,5 +1,5 @@
-using System.Diagnostics;
 using AdventOfCode.Abstractions;
+using AdventOfCode.Year2022.Day02.RockPaperScissors;
 
 namespace AdventOfCode.Year2022.Day02;
 
@@ -9,8 +9,18 @@ public sealed class Day02Solver : DaySolver
 	public override int Day => 2;
 	public override string Title => "Rock Paper Scissors";
 
+	private readonly EncryptedStrategyGuide _encryptedStrategyGuide;
+
 	public Day02Solver(Day02SolverOptions options) : base(options)
 	{
+		try
+		{
+			_encryptedStrategyGuide = EncryptedStrategyGuide.Parse(Input);
+		}
+		catch (FormatException e)
+		{
+			throw new InputException(e.Message);
+		}
 	}
 
 	public Day02Solver(Action<Day02SolverOptions> configure)
@@ -22,101 +32,15 @@ public sealed class Day02Solver : DaySolver
 	{
 	}
 
-	private int CalculateScore1(string s)
-	{
-		string[] parts = s.Split();
-		string elf = parts[0] switch
-		{
-			"A" => "rock",
-			"B" => "paper",
-			"C" => "scissors",
-			_ => throw new InputException($"Unknown elf type \"{parts[0]}\".")
-		};
-		string mine = parts[1] switch
-		{
-			"X" => "rock",
-			"Y" => "paper",
-			"Z" => "scissors",
-			_ => throw new InputException($"Unknown mine type \"{parts[1]}\".")
-		};
-		int score = mine switch
-		{
-			"rock" => 1,
-			"paper" => 2,
-			"scissors" => 3,
-			_ => throw new UnreachableException($"Unknown mine type \"{mine}\".")
-		};
-		score += (mine, elf) switch
-		{
-			("rock", "rock") => 3,
-			("rock", "paper") => 0,
-			("rock", "scissors") => 6,
-			("paper", "rock") => 6,
-			("paper", "paper") => 3,
-			("paper", "scissors") => 0,
-			("scissors", "rock") => 0,
-			("scissors", "paper") => 6,
-			("scissors", "scissors") => 3,
-			_ => throw new UnreachableException($"Unknown mine type \"{mine}\" and elf type \"{elf}\".")
-		};
-		return score;
-	}
-
-	private int CalculateScore2(string s)
-	{
-		string[] parts = s.Split();
-		string elf = parts[0] switch
-		{
-			"A" => "rock",
-			"B" => "paper",
-			"C" => "scissors",
-			_ => throw new InputException($"Unknown elf type \"{parts[0]}\".")
-		};
-		string mine = (elf, parts[1]) switch
-		{
-			("rock", "X") => "scissors",
-			("paper", "X") => "rock",
-			("scissors", "X") => "paper",
-			("rock", "Y") => "rock",
-			("paper", "Y") => "paper",
-			("scissors", "Y") => "scissors",
-			("rock", "Z") => "paper",
-			("paper", "Z") => "scissors",
-			("scissors", "Z") => "rock",
-			_ => throw new InputException($"Unknown mine type \"{parts[1]}\".")
-		};
-		int score = mine switch
-		{
-			"rock" => 1,
-			"paper" => 2,
-			"scissors" => 3,
-			_ => throw new UnreachableException($"Unknown mine type \"{mine}\".")
-		};
-		score += (mine, elf) switch
-		{
-			("rock", "rock") => 3,
-			("rock", "paper") => 0,
-			("rock", "scissors") => 6,
-			("paper", "rock") => 6,
-			("paper", "paper") => 3,
-			("paper", "scissors") => 0,
-			("scissors", "rock") => 0,
-			("scissors", "paper") => 6,
-			("scissors", "scissors") => 3,
-			_ => throw new UnreachableException($"Unknown mine type \"{mine}\" and elf type \"{elf}\".")
-		};
-		return score;
-	}
-
 	public override string SolvePart1()
 	{
-		int sum = InputLines.Select(CalculateScore1).Sum();
-		return $"{sum}";
+		int result = _encryptedStrategyGuide.CalculateScoresAssumingValues().Sum();
+		return $"{result}";
 	}
 
 	public override string SolvePart2()
 	{
-		int sum = InputLines.Select(CalculateScore2).Sum();
-		return $"{sum}";
+		int result = _encryptedStrategyGuide.CalculateScoresAssumingResults().Sum();
+		return $"{result}";
 	}
 }
