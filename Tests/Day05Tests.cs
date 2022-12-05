dbd66f59abc17f1b18ce38669e693a427e4de993
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-12-05 20:56:36 +0100

    Added day 5 example input tests

--- Tests/Day05Tests.cs
+++ Tests/Day05Tests.cs
@@ -13,11 +13,13 @@ public class Day05Tests : BaseDayTests<Day05Solver, Day05SolverOptions>
 	protected override Day05Solver CreateSolver(Day05SolverOptions options) => new(options);
 
 	[Theory]
+	[InlineData("example-input.txt", "CMZ")]
 	[InlineData("my-input.txt", "CFFHVVHNC")]
 	public override void TestPart1(string inputFilename, string expectedResult, Day05SolverOptions? options = null)
 		=> base.TestPart1(inputFilename, expectedResult, options);
 
 	[Theory]
+	[InlineData("example-input.txt", "MCD")]
 	[InlineData("my-input.txt", "FSZWBPTBG")]
 	public override void TestPart2(string inputFilename, string expectedResult, Day05SolverOptions? options = null)
 		=> base.TestPart2(inputFilename, expectedResult, options);
