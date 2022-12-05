d734127a87a819d8622203632e7d58c204c95eb5
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-12-05 10:24:46 +0100

    Added day 5 only my-input tests

---
+++ Tests/Day05Tests.cs
@@ -0,0 +1,24 @@
+using AdventOfCode.Year2022.Day05;
+using Xunit;
+
+namespace AdventOfCode.Year2022.Tests;
+
+[Trait("Year", "2022")]
+[Trait("Day", "05")]
+[Trait("Day", "5")]
+public class Day05Tests : BaseDayTests<Day05Solver, Day05SolverOptions>
+{
+	public override string DayInputsDirectory => "Day05";
+
+	protected override Day05Solver CreateSolver(Day05SolverOptions options) => new(options);
+
+	[Theory]
+	[InlineData("my-input.txt", "CFFHVVHNC")]
+	public override void TestPart1(string inputFilename, string expectedResult, Day05SolverOptions? options = null)
+		=> base.TestPart1(inputFilename, expectedResult, options);
+
+	[Theory]
+	[InlineData("my-input.txt", "FSZWBPTBG")]
+	public override void TestPart2(string inputFilename, string expectedResult, Day05SolverOptions? options = null)
+		=> base.TestPart2(inputFilename, expectedResult, options);
+}
